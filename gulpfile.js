177554a6b6dd7bddeef6f714c28837306b930e93
Emil Palm <emilpalm94@gmail.com>
2021-03-19 15:34:22 +0100

    commit for babel moment 4

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const uglifycss = require("gulp-uglifycss");
 const sass = require('gulp-sass'); 
 sass.compiler = require('node-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const babel = require("gulp-babel");
 
 // Sökvägar
 
@@ -19,6 +20,17 @@ const files = {
 function copyHTML() {
     return src(files.htmlPath)
     .pipe(dest("pub")
+    );
+}
+
+// Babel
+function babelTask() {
+    return src(files.jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("main.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest('pub/js')
     );
 }
 
@@ -57,12 +69,12 @@ function sassTask() {
 //watcher
 function watchTask() {
     watch([files.htmlPath, files.jsPath, files.sassPath, files.cssPath], 
-        parallel(copyHTML, jsTask, sassTask, cssTask));
+        parallel(copyHTML, jsTask, sassTask, cssTask, babelTask));
 }
 
 
 //Default Task
 exports.default = series(
-    parallel(copyHTML, jsTask, sassTask, cssTask),
+    parallel(copyHTML, jsTask, sassTask, cssTask, babelTask),
     watchTask
 );
